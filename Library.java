1ca0b5811a8bb500373adabbe1ceb9611cceff51
Emaan Fatima <emaanfatima0613@gmail.com>
2024-10-05 11:02:01 +0500

    Added searchBook function in library class

--- Library.java
+++ Library.java
@@ -4,13 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Library {
-private List<String> books = new ArrayList<>();
+	private List<String> books = new ArrayList<>();
 
-public void addBook(String bookName) {
-books.add(bookName);
-System.out.println(bookName + " added to library.");
+	public void addBook(String bookName) {
+		books.add(bookName);
+		System.out.println(bookName + " added to library.");
+	}
+
+	public void listBooks() {
+		System.out.println("Books in library: " + books);
+	}
+
+	public boolean searchBook(String bookName) {
+		return books.contains(bookName);
+	}
+
 }
-
-public void listBooks() {
-System.out.println("Books in library: " + books);
-} }
